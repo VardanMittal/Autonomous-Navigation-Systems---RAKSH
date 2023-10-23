2c5979b288342c976cfc00a8bc61bb43edb5e553
Vardan Mittal <vardanmittal000@gmail.com>
2023-10-23 12:19:09 +0530

    SPI transfer codes added
    "SPI master and slave codes are written slight modification are required as per use case"

--- ESP32_codes/Spi_Slave/Spi_Slave.ino
+++ ESP32_codes/Spi_Slave/Spi_Slave.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#define slaveSelect 2
 
+int delayTime = 50;
+float receivedDistance, receivedVelocity;
+char receivedDirection;
+
+void setup() {
+  pinMode(slaveSelect, OUTPUT);
+  SPI.begin();
+  SPI.setBitOrder(LSBFIRST);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Receiving data from Jetson Nano
+  digitalWrite(slaveSelect, LOW);
+  receivedDistance = SPI.transfer(0);
+  receivedVelocity = SPI.transfer(0);
+  receivedDirection = SPI.transfer(0);
+  digitalWrite(slaveSelect, HIGH);
 
+  // Process the received data
+
+  // Sending data to Jetson Nano
+  int acceleration[3] = {1, 2, 3};
+  int velocity[3] = {4, 5, 6};
+  int position[3] = {7, 8, 9};
+  int angles[3] = {10, 11, 12};
+
+  digitalWrite(slaveSelect, LOW);
+  SPI.transfer(acceleration, 3);
+  SPI.transfer(velocity, 3);
+  SPI.transfer(position, 3);
+  SPI.transfer(angles, 3);
+  digitalWrite(slaveSelect, HIGH);
+
+  delay(delayTime);
 }
--- Jetson_nano_Codes/SpiConnections/SPI_Slave.py
+++ Jetson_nano_Codes/SpiConnections/SPI_Slave.py
@@ -1,6 +1,28 @@
-data_dict = {
-    #we need information regarding all 6 dof
-    # any sensory inputs we are getting from the imu ?? is just for motor controlling ?
-}
+import spidev
+import time
 
-import scipy
+spi = spidev.SpiDev()
+spi.open(0, 0)  # 0, 0 represents the bus and device (CS pin)
+
+# Example values to be sent
+distance = 1.5
+velocity = 2.5
+direction = ord('N')
+
+# Sending data to ESP32
+spi.xfer([int(distance * 100), int(velocity * 100), direction])
+
+# Receiving data from ESP32
+received_data = spi.readbytes(12)  # 3 integers * 4 bytes each
+
+# Converting received data back to integer format
+acceleration = [int(received_data[i]) for i in range(0, 3)]
+received_velocity = [int(received_data[i + 3]) for i in range(0, 3)]
+position = [int(received_data[i + 6]) for i in range(0, 3)]
+angles = [int(received_data[i + 9]) for i in range(0, 3)]
+
+# Processing the received data
+
+# Add your processing code here
+
+time.sleep(0.05)  # Adjust the delay as needed
